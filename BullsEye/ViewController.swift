67ab901b8d78b1820927a633b6d31eb235c4b4d8
prestonjarnagin <prstnjarnagin@gmail.com>
2019-06-03 13:35:08 -0600

    Initial Commit

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        targetValue = Int.random(in: 1...100)
+        startNewRound()
     }
 
-
+    @IBAction func showAlert(){
+        var difference: Int = currentValue - targetValue
+        if difference < 0 {
+            difference *= -1
+        }
+        
+        let message = "Slider: \(currentValue)" +
+            "\nThe target is: \(targetValue)" +
+            "\nThe difference was \(difference)"
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        startNewRound()
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider){
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+    }
+    
+    func startNewRound(){
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
+    }
 }
 
